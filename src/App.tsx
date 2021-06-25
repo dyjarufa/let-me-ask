3e4cfe068d52111398ed39235753f2c45dd4bbb9
Jady Rufino <jadyrufa@gmail.com>
2021-06-24 23:47:10 -0300

    reafactor: add hook AuthContext

--- src/App.tsx
+++ src/App.tsx
@@ -2,72 +2,17 @@ import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
 import { Route, BrowserRouter } from 'react-router-dom';
-import { createContext, useState } from 'react';
-import { auth, firebase } from './services/firebase';
-import { useEffect } from 'react';
-
-type User = {
-  id: string;
-  name: string;
-  avatar: string;
-}
-
-type AuthContextType = {
-  user: User | undefined;
-  signInWithGoogle: () => Promise<void>; //como mudei a função apara async, ela devolve uma Promise o seu retorno será void
-}
-
-export const AuthContext = createContext({} as AuthContextType);
+import { AuthContextProvider } from './contexts/AuthContext';
 
 function App() {
-const [user, setUser] = useState<User>();
-
-useEffect(() => {
-  //onAuthStateChanged state change. Ouve o evento
-  //ele detecta se um usuário já tinha logado na aplicação anteriormente
-  auth.onAuthStateChanged(user => { 
-    if(user){
-      const { displayName, photoURL, uid } = user;
-
-      if(!displayName || !photoURL){
-        throw new Error('Missing information from Google Account');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-      })
-    }
-  })
-}, [])
-
-async function signInWithGoogle(){
-  const provider = new firebase.auth.GoogleAuthProvider();
-
-  const result = await auth.signInWithPopup(provider)
-    if(result.user){
-      const { displayName, photoURL, uid } = result.user;
-
-      if(!displayName || !photoURL){
-        throw new Error('Missing information from Google Account');
-      }
-
-      setUser({
-        id: uid,
-        name: displayName,
-        avatar: photoURL
-      })
-    }
-}
 
   return (
-    <AuthContext.Provider value={{ user, signInWithGoogle }}>
-      <BrowserRouter>
+    <BrowserRouter>
+    <AuthContextProvider>
         <Route path="/" exact component={Home}/>
         <Route path="/rooms/new" component={NewRoom}/>
+    </AuthContextProvider>
       </BrowserRouter>
-    </AuthContext.Provider>
   );
 }
 
--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -5,12 +5,11 @@ import { Link } from 'react-router-dom';
 
 import '../styles/auth.scss'
 import { Button } from '../components/Button'; 
-import { useContext } from 'react';
-import { AuthContext } from '../App';
+import { useAuth } from '../hooks/useAuth';
 
 
  export function NewRoom() {
-   const { user } = useContext(AuthContext);
+  //  const { user } = useAuth();
 
   return(
     <div id="page-auth">
@@ -22,7 +21,6 @@ import { AuthContext } from '../App';
       <main>
           <div className="main-content">
             <img src={logoImg} alt="letmeask" />
-            <h1>{user?.name}</h1>
             <h2>Create a new room</h2>
             <form>
               <input 
