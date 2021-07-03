2984bfd3b0c910a954eee0a37cadeddcb9533ef8
Jady Rufino <jadyrufa@gmail.com>
2021-07-03 19:51:30 -0300

    create rule to ask question and store on firebase

--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,9 +1,12 @@
+import { FormEvent, useState } from 'react';
 import { useParams } from 'react-router'; 
 
 import logoImg from '../assets/images/logo.svg';
 
 import { Button } from '../components/Button';
 import { RoomCode } from '../components/RoomCode';
+import { useAuth } from '../hooks/useAuth';
+import { database } from '../services/firebase';
 
 import '../styles/room.scss'
 
@@ -14,13 +17,45 @@ type RoomParams = {
 export function Room(){
   //Consigo recuperar os parâmtros da rota da minha página(App.tsx) com o useParams.
   const params = useParams<RoomParams>();
+  const roomId = params.id;
+
+  const { user } = useAuth();
+
+  const [newQuestion, setNewQuestion] = useState('')
+
+  async function handleSendQuestion(event: FormEvent){
+    event.preventDefault();
+
+    if(newQuestion.trim() === '') {
+      return;
+    }
+
+    if(!user){
+      //TODO - hot toast
+      throw new Error('You must be logged in')
+    }
+
+    const question = {
+      content: newQuestion,
+      author: {
+        name: user.name,
+        avatar: user.avatar,
+      },
+      isHighlighted: false,
+      isAnswered: false
+    }
+
+    await database.ref(`rooms/${roomId}/questions`).push(question); //será criando o path questions dentro do firebase
+    setNewQuestion('')
+
+  }
   
   return(
     <div id="page-room"> 
       <header>
         <div className="content">
           <img src={logoImg} alt="letmeask" />
-          <RoomCode code={params.id}/>
+          <RoomCode code={roomId}/>
         </div>
       </header>
 
@@ -30,14 +65,25 @@ export function Room(){
           <span>4 questions</span>
         </div>
 
-        <form>
+        <form onSubmit={handleSendQuestion}>
           <textarea 
             placeholder="what would you like to ask?"
+            onChange={event => setNewQuestion(event.target.value)}
+            value={newQuestion}
           />
 
           <div className="form-footer">
-            <span>To ask a question, <button>please login</button>.</span>
-            <Button type="submit">Send question</Button>  
+            {user ? (
+              <div className="user-info">
+                <img src={user.avatar} alt={user.name} />
+                <span>{user.name}</span>
+              </div>
+            ) : (
+              <span>To ask a question, <button>please login</button>.</span>
+            ) }
+            <Button type="submit" disabled={!user}>
+                Send question
+            </Button>  
           </div>
         </form>
       </main>
