129dd65d9eaad3a2429883525a6e3057c7e46deb
Jady Rufino <jadyrufa@gmail.com>
2021-08-09 22:30:05 -0300

    add like function

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,4 +1,3 @@
-import { FormEvent, useEffect, useState } from 'react';
 import { useParams } from 'react-router'; 
 
 import logoImg from '../assets/images/logo.svg';
@@ -6,9 +5,8 @@ import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
 import { Question } from '../components/Question';
 import { RoomCode } from '../components/RoomCode';
-import { useAuth } from '../hooks/useAuth';
+// import { useAuth } from '../hooks/useAuth';
 import { useRoom } from '../hooks/useRoom';
-import { database } from '../services/firebase';
 
 import '../styles/room.scss'
 
@@ -18,40 +16,11 @@ type RoomParams = {
 
 export function AdminRoom(){
   //Consigo recuperar os parâmtros da rota da minha página(App.tsx) com o useParams.
-  const { user } = useAuth();
+  // const { user } = useAuth();
   const params = useParams<RoomParams>();
   const roomId = params.id;
 
-  const [newQuestion, setNewQuestion] = useState('')
   const {questions, title} = useRoom(roomId);
-  
-
-  async function handleSendQuestion(event: FormEvent){
-    event.preventDefault();
-
-    if(newQuestion.trim() === '') {
-      return;
-    }
-
-    if(!user){
-      //TODO - hot toast
-      throw new Error('You must be logged in')
-    }
-
-    const question = {
-      content: newQuestion,
-      author: {
-        name: user.name,
-        avatar: user.avatar,
-      },
-      isHighlighted: false,
-      isAnswered: false
-    }
-
-    await database.ref(`rooms/${roomId}/questions`).push(question); //será criando o path questions dentro do firebase
-    setNewQuestion('')
-
-  }
   
   return(
     <div id="page-room"> 
